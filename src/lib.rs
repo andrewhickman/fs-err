724e1ac0351f8101dc963eeb7a2e1812a9abbdb4
Andrew Hickman <andrew.hickman1@sky.com>
2020-02-02 20:44:18 +0000

    Add raw operations

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,36 @@
 #![doc(html_root_url = "https://docs.rs/fs-err/0.1.0")]
 #![deny(missing_debug_implementations, missing_docs)]
 
+use std::path::{Path, PathBuf};
+
 /// A wrapper around a file handle and its path which wraps all
 /// operations with more helpful error messages.
 #[derive(Debug)]
-pub struct File;
+pub struct File {
+    file: std::fs::File,
+    path: PathBuf,
+}
+
+impl File {
+    /// Creates a [`File`](struct.File.html) from a raw file and its
+    /// path.
+    pub fn from_parts<P>(file: std::fs::File, path: P) -> Self
+    where
+        P: Into<PathBuf>,
+    {
+        File {
+            file,
+            path: path.into(),
+        }
+    }
+
+    /// Gets the wrapped file.
+    pub fn file(&self) -> &std::fs::File {
+        &self.file
+    }
+
+    /// Gets the path of the wrapped file.
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
