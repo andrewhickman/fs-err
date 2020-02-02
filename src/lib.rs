009df7e4c689d0d7181571e5f2115dc6a8a9992c
Andrew Hickman <andrew.hickman1@sky.com>
2020-02-02 21:00:09 +0000

    Add Error

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![doc(html_root_url = "https://docs.rs/fs-err/0.1.0")]
 #![deny(missing_debug_implementations, missing_docs)]
 
+use std::fmt;
 use std::path::{Path, PathBuf};
 
 /// A wrapper around a file handle and its path which wraps all
@@ -13,6 +14,14 @@ use std::path::{Path, PathBuf};
 pub struct File {
     file: std::fs::File,
     path: PathBuf,
+}
+
+/// A wrapper around a `std::io::Error` from a filesystem operation,
+/// with context including the path of the associated file.
+#[derive(Debug)]
+pub struct Error {
+    source: std::io::Error,
+    message: String,
 }
 
 impl File {
@@ -36,5 +45,29 @@ impl File {
     /// Gets the path of the wrapped file.
     pub fn path(&self) -> &Path {
         &self.path
+    }
+}
+
+impl Error {
+    /// Constructs an [`Error`](struct.Error.html).
+    pub fn new(source: std::io::Error, message: String) -> Self {
+        Error { source, message }
+    }
+
+    /// Gets a reference to the raw error.
+    pub fn source(&self) -> &std::io::Error {
+        &self.source
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.message.fmt(f)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self.source())
     }
 }
