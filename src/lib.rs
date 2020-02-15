af2cdde1b0c21b2fc56a6ab5a65ca6611fcd1557
Andrew Hickman <andrew.hickman1@sky.com>
2020-02-15 13:32:14 +0000

    Version 1.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! around a file handle and its path which wraps all operations with
 //! more helpful error messages.
 
-#![doc(html_root_url = "https://docs.rs/fs-err/1.0.0")]
+#![doc(html_root_url = "https://docs.rs/fs-err/1.0.1")]
 #![deny(missing_debug_implementations, missing_docs)]
 
 use std::fmt;
