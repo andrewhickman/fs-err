ba9888793c517a82499c8df1ad013529d8578777
Andrew Hickman <andrew.hickman1@sky.com>
2023-11-20 18:28:19 +0000

    chore: Release fs-err version 2.11.0

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ println!("Program config: {:?}", decoded);
 [serde_json]: https://crates.io/crates/serde_json
 */
 
-#![doc(html_root_url = "https://docs.rs/fs-err/2.10.0")]
+#![doc(html_root_url = "https://docs.rs/fs-err/2.11.0")]
 #![deny(missing_debug_implementations, missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
