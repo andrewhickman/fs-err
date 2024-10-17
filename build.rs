2c3a3c53796f2cf00c89fa7b5389b742f6edaddb
Schneems <richard.schneeman+foo@gmail.com>
2024-10-17 14:34:04 -0500

    Re-run build.rs if the file contents change

--- build.rs
+++ build.rs
@@ -4,4 +4,7 @@ fn main() {
     let ac = autocfg::new();
     // Allows `#[cfg(rustc_1_63)]` to be used in code
     ac.emit_rustc_version(1, 63);
+
+    // Re-run if this file changes
+    autocfg::rerun_path("build.rs");
 }
