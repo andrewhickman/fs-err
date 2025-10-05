b212a7f19d77f8195e7995e4f1b0b995fbb9a438
xzfc <5121426+xzfc@users.noreply.github.com>
2025-10-05 12:40:11 +0000

    Add `std::fs::exists` and `tokio::fs::try_exists` (#77)
    * feat: wrap `std::fs::exists`
    
    * feat: wrap `tokio::fs::try_exists`

--- build.rs
+++ build.rs
@@ -2,9 +2,9 @@ extern crate autocfg;
 
 fn main() {
     let ac = autocfg::new();
-    // Allows `#[cfg(rustc_1_63)]` and `#[cfg(rustc_1_89)]` to be used in code
-    ac.emit_rustc_version(1, 63);
-    ac.emit_rustc_version(1, 89);
+    ac.emit_rustc_version(1, 63); // #[cfg(rustc_1_63)]
+    ac.emit_rustc_version(1, 81); // #[cfg(rustc_1_81)]
+    ac.emit_rustc_version(1, 89); // #[cfg(rustc_1_89)]
 
     // Re-run if this file changes
     autocfg::rerun_path("build.rs");
